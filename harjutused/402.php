24a55a33250e1730ed4f9ad418c21da4357245b9
NikolaiGrigorjev <topclash765@gmai.com>
2021-11-09 10:55:32 +0200

    Ülesanne 402 on tehtud

---
+++ harjutused/402.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Ülessane 403</title>
+</head>
+<body>
+<a href="../phptest.php">tagasi.....Ülesannete leht</a>
+<h1>Ülesanne 402</h1>
+<h2>1.20 Checbox tsikliiga</h2>
+<?php
+    $i=1;
+    while($i<20){
+        echo "<input type='checkbox' id='$i' name='box[]' value ='$i'>";
+        echo "<label for='$i'>box ".$i."</label><br>";
+        $i++;
+
+    }
+
+?>
+<br>
+<h2>2.20 radionupp tsikliiga</h2>
+<?php
+$r=1;
+while($r<20){
+    echo "<input type='radio' id='l$r' name='radio[]' value ='$r'>";
+    echo "<label for='l$r'>value ".$r."</label><br>";
+    $r++;
+
+}
+
+?>
+
+<h3>3.20 tekstbox tsikliiga</h3>
+<?php
+$q=1;
+while($q<20){
+    echo "<input type='textbox' id='u$q' name='tekst[]' value ='$q'>";
+    echo "<label for='u$q'>cell ".$q."</label><br>";
+    $q++;
+
+}
+
+?>
+
+</body>
+</html>
+
